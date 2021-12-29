db98e5de7585707578ebee48faa7b3a022afdac3
freelancerbanna <hasanulhaquebanna@gmail.com>
2021-12-29 18:30:53 +0600

    last commit

--- pages/verify.js
+++ pages/verify.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { auth, provider } from "../../firebase/firebase";
+import { auth, provider } from "../firebase/firebase";
 import { signInWithPopup } from "firebase/auth";
 const UserVerify = () => {
   const router = useRouter();
